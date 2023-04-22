a5526208110762c21b8be7d55a79c07da24a15b4
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2023-04-22 21:54:28 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -13,26 +13,37 @@ def get_data():
     end = request.args.get('end')
     limit = request.args.get('limit')
 
-    response = requests.get(json_url)
-    json_data = json.loads(response.text)
+    try:
+        response = requests.get(json_url)
+        response.raise_for_status()
+        json_data = response.json()
 
-    if start:
-        start = int(start)
-    else:
-        start = 0
+        if start:
+            start = int(start)
+        else:
+            start = 0
 
-    if end:
-        end = int(end)
-    else:
-        end = len(json_data)
+        if end:
+            end = int(end)
+        else:
+            end = len(json_data)
 
-    if limit:
-        limit = int(limit)
-        end = start + limit
+        if limit:
+            limit = int(limit)
+            end = start + limit
 
-    response_data = json_data[start:end]
-    response = Response(json.dumps(response_data), mimetype='application/json')
-    return response
+        response_data = json_data[start:end]
+        response = Response(json.dumps(response_data), mimetype='application/json')
+        return response
+
+    except requests.exceptions.RequestException as e:
+        error_msg = f"Error retrieving data: {e}"
+        return Response(json.dumps({"error": error_msg}), status=500, mimetype='application/json')
+
+    except Exception as e:
+        error_msg = f"An error occurred: {e}"
+        return Response(json.dumps({"error": error_msg}), status=500, mimetype='application/json')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=10000)
